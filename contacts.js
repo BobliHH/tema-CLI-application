864b54634c4f2f59e735ba1cb30b8633ef8024ce
=Andrei <balasca.andrei@gmail.com>
2023-09-19 14:38:26 +0300

    tried something but result is empty obj

---
+++ contacts.js
@@ -0,0 +1,34 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactPath = path.join(__dirname, "db", "contacts.json");
+console.log(contactPath);
+
+function listContacts() {
+  const list = fs.readFile(contactPath);
+  return JSON.parse(list);
+}
+
+function getContactById(contactId) {
+  const contacts = listContacts();
+  return contacts[contactId]; // returns undefined if not found!
+}
+
+function removeContact(contactId) {
+  const contacts = listContacts();
+  delete contacts[contactId];
+  console.log("delete", contacts);
+}
+
+function addContact(name, email, phone) {
+  const contacts = listContacts();
+  let newContact = {};
+  newContact["id"] = Math.floor(Math.random() * 10 + 5)
+    .toString()
+    .padStart("2", "0");
+  newContact["name"] = name || "";
+  newContact["email"] = email || "";
+  newContact["phone"] = phone || "";
+  console.log("newContact", newContact);
+  return newContact;
+}
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log(process);
+// console.log(process);
+const contacts = require("./contacts");
+const list = contacts.listContacts;
+console.log(contacts);
+console.log(list);
+console.log();
